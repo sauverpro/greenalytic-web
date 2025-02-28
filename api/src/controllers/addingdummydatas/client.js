e0bd388ce7d59acaae7a6afa1ceff6731e5fef89
IMANARIYO <imanariyobaptiste@gmail.com>
2025-02-28 23:04:34 +0200

    removing the map  since  even it  was showing the   dummmy datas

--- api/src/controllers/addingdummydatas/client.js
+++ api/src/controllers/addingdummydatas/client.js
@@ -1,7 +1,8 @@
 import io from 'socket.io-client'
 
-
-const socket = io(`http://localhost:${process.env.EXPRESS_SERVER_PORT}`) // Replace with your actual server URL
+// const socket = io(`http://localhost:${process.env.EXPRESS_SERVER_PORT}`)
+// // Replace with your actual server URL
+const socket = io('https://heptadevwepapi.onrender.com') // Replace with your actual server URL
 
 // Car's initial data
 let carData = {
