29098ca9f97654a830817560d84ede81481071d0
Bananayosostene <sbananayo98@gmail.com>
2025-05-14 08:59:19 +0200

    fi UI block after adding device

--- frontend/src/app/(admin)/admin/users/page.tsx
+++ frontend/src/app/(admin)/admin/users/page.tsx
@@ -15,67 +15,12 @@ import {
 } from "./_components/ExportUtils";
 import type { User } from "@/types/types";
 
-import EditUserDrawer from "./_components/EditUserDrawer";
 
 import { toast } from "sonner";
-import {
-  AlertDialog,
-  AlertDialogAction,
-  AlertDialogCancel,
-  AlertDialogContent,
-  AlertDialogDescription,
-  AlertDialogFooter,
-  AlertDialogHeader,
-  AlertDialogTitle,
-  AlertDialogTrigger
-} from "@/components/ui/alert-dialog";
-import { Button } from "@/components/ui/button";
+
 import ViewUserDrawer from "./_components/ViewUserDrawer";
-import AddUserDrawer from "./_components/AddUserDrawer";
 import UserDrawer from "@/components/UserDrawer";
 
-export function ConfirmAndToastDialog({
-  onConfirm
-}: {
-  onConfirm: () => void;
-}) {
-  const [open, setOpen] = useState(false);
-
-  const handleConfirm = () => {
-    setOpen(false);
-    onConfirm();
-    toast("Action Confirmed", {
-      description: "Your settings have been saved successfully.",
-      action: {
-        label: "Undo",
-        onClick: () => console.log("Undo clicked")
-      }
-    });
-  };
-
-  return (
-    <AlertDialog open={open} onOpenChange={setOpen}>
-      <AlertDialogTrigger asChild>
-        <Button variant="outline">Show Dialog</Button>
-      </AlertDialogTrigger>
-      <AlertDialogContent>
-        <AlertDialogHeader>
-          <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
-          <AlertDialogDescription>
-            This action cannot be undone. It will permanently remove the
-            selected data.
-          </AlertDialogDescription>
-        </AlertDialogHeader>
-        <AlertDialogFooter>
-          <AlertDialogCancel>Cancel</AlertDialogCancel>
-          <AlertDialogAction asChild>
-            <Button onClick={handleConfirm}>Continue</Button>
-          </AlertDialogAction>
-        </AlertDialogFooter>
-      </AlertDialogContent>
-    </AlertDialog>
-  );
-}
 
 export default function UsersPage() {
   const [selectedUser, setSelectedUser] = useState<User | null>(null);
