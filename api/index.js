1a06a0be1135cc14608a8bd8df74c29295bc87f3
IMANARIYO <imanariyobaptiste@gmail.com>
2025-07-02 14:10:55 +0200

    removing un  known devices

--- api/index.js
+++ api/index.js
@@ -232,8 +232,7 @@ console.log('GPS Data saved:', gps)
 // Start the server
 const expressPort = process.env.EXPRESS_SERVER_PORT || 5000
 server.listen(expressPort, async () => {
- 
-  console.log('Tracking Devices:',devices)
+
   console.log(`Server running on http://localhost:${expressPort}/api-docs`)
 })
 
